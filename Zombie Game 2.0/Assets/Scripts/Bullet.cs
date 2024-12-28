6e560ad04e677e4745c384406c7c13972be39529
shaneypricken <162596806+shaneypricken@users.noreply.github.com>
2024-12-27 23:09:16 -0500

    cant figure out bullet rotation

--- Zombie Game 2.0/Assets/Scripts/Bullet.cs
+++ Zombie Game 2.0/Assets/Scripts/Bullet.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
+   
+
+    void Update() {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         print("Hit" + other.name);
